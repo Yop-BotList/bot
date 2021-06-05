78e683b7cff8ea485985d1912badeab603e7a7b7
Nonolanlan1007 <84439677+Nonolanlan1007@users.noreply.github.com>
2021-06-05 10:38:45 +0200

    Ajout des logs de commandes

--- events/message.js
+++ events/message.js
@@ -1,4 +1,5 @@
-const config = require('../configs/config.json');
+const config = require('../configs/config.json'),
+      { botlogs } = require('../configs/channels.json');
 
 module.exports = (client, message) =>  {
     if (message.content.indexOf(client.prefix) !== 0) return;
@@ -10,6 +11,31 @@ module.exports = (client, message) =>  {
     if (cmd) {
         if (message.author.bot && config.botsAllowed === false) return message.channel.send('**' + client.no + ' | Désolé, mais je suis configuré pour ne pas répondre aux autres bots.**');
         if (message.channel.type === 'dm') return message.channel.send(client.no + " | Désolé, mais mes commandes sont utilisables uniquement dans un serveur.")
+        client.channels.cache.get(botlogs).send({
+            embed: {
+                title: "Utilisation d'un commande",
+                thumbnail: client.user.displayAvatarURL(),
+                color: client.color,
+                timestamp: new Date(),
+                fields: [
+                    {
+                        name: "❱ Utilisateur :",
+                        value: `\`\`\`${message.author.username}#${message.author.discriminator} (${message.author.id})\`\`\``,
+                        inline: false
+                    },
+                    {
+                        name: "❱ Commande :",
+                        value: "```" + message.content + "```",
+                        inline: false
+                    },
+                    {
+                        name: "❱ Lien",
+                        value: `[Cliquez-ici](https://discord.com/channels/${message.guild.id}/${message.channel.id}/${message.id}) _Il se peut que cette personne aie supprimé ou édité son message._`,
+                        inline: false
+                    }
+                ]
+            }
+        })
         cmd.run(client, message, args)
     }
 }
