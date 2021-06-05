01218a8dd61bae32655251b3e15ff546e82f0235
Nonolanlan1007 <84439677+Nonolanlan1007@users.noreply.github.com>
2021-06-05 11:14:35 +0200

    Delete commands directory

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-exports.run = async (client, message) => {
-    await message.channel.send('Pong :ping_pong:').then(msg => {
-        msg.edit(`Pong :ping_pong: \`${Math.sqrt(((new Date() - message.createdTimestamp)/(5*2))**2)} ms\``)
-    });
-}
-
-exports.help = {
-    name: "ping"
-}
