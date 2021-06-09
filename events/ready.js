e99fc5457065f54ffe0b9eb8128577f11d85a6c0
Nonolanlan1007 <84439677+Nonolanlan1007@users.noreply.github.com>
2021-06-09 19:00:15 +0200

    Ajout d'un statut au bot

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,12 @@ const { blue } = require('colors'),
       { online } = require('../configs/emojis.json');
 
 module.exports = (client) => {
-    console.log(blue(`Je vient bien de me connecter à Discord !`) + "\n==================================================================")
+    console.log(blue(`Je vient bien de me connecter en tant que ${client.user.tag} !`) + "\n==================================================================")
     client.channels.cache.get(botlogs).send("**" + online + " | Je viens tout juste de me connecter !**")
+    
+    client.user.setActivity('hémerger d\'un profond sommeil...');
+    const activities = [`${client.prefix}help | By Nolhan#2508`, `Version 1.8 | By Nolhan#2508`];
+    setInterval(async () => {
+            await client.user.setActivity(activities[Math.floor(Math.random() * activities.length)]);
+            }, 120000);
 }
