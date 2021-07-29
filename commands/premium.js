512a9633d542b93308dd3f224dcc5fe28a080263
Nonolanlan1007 <84439677+Nonolanlan1007@users.noreply.github.com>
2021-07-29 19:01:54 +0200

    Update premium.js

--- commands/premium.js
+++ commands/premium.js
@@ -10,12 +10,12 @@ exports.run = async (client, message) => {
             fields: [
                 {
                     name: 'Obtention :',
-                    value: 'Le grade <@&783375866452508692> ne peut pas être obtenu gratuitement. \nVous pouvez avoir ce rôle en invitant 10 personnes (_voir `+ranks`_) ou en "bumpant" le serveur 15 fois avec la commande `!d bump` (_voir `br.ranks`_).',
+                    value: "Le grade <@&783375866452508692> ne peut pas être obtenu gratuitement.\n Pour l'obtenir, vous devez le gagner lors d'un giveaway. Si vous êtes l'un de nos <@&784745567032573973> ou <@&828572424211398716> *(voir <#828531510772367391>)*, vous bénéfieciez aussi des avantages !",
                     inline: false,
                 },
                 {
                     name: 'Avantages :',
-                    value: '• Mention supplémentaire au bout d’une semaine d’ajout.\n• 2 bots.\n• Ajout d’un salon d’annonces dans le <#783013505225719829>.\n• Le grade <@&793543495599915069> pour tout vos bots.',
+                    value: '• 2 bots.\n• Ajout d’un salon d’annonces dans le <#783013505225719829>*(seulement pour les 10 premiers)*.\n• Le grade <@&793543495599915069> pour tout vos bots.\n• La possibilité de changer son pseudo.\n• La possibilité de créer des threads dans certains salons.',
                     inline: false,
                 }
             ],
