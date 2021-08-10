110d224df5fa7d702aedea8a52c38da61ec61aff
Nonolanlan1007 <84439677+Nonolanlan1007@users.noreply.github.com>
2021-08-10 15:20:53 +0200

    Optimisation de l'autorole + ajout de l'aurename

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,9 +8,10 @@ module.exports = (client, member) => {
     }
     if (member.user.bot) {
         // roles
-            member.roles.add(botintests);
+            if (client.dbProprio.has(`Proprio_${member.user.id}`)) member.roles.add(botintests);
             member.roles.add(botrole);
-
+        // autorename
+        if (client.dbPrefix.has(`Prefix_${member.user.id}`)) member.setNickname(`[${client.dbPrefix.get(`Prefix_${member.user.id}`)}] ${member.user.username}`)
         // message
         client.channels.cache.get(welcomechannel).send(`**<a:entre:838336027616739338> | Oh non, de la concurrance <:Panda_horrible:838335972931272745> ! Nan, je rigole ! Bienvenue à toi <@${member.user.id}> :wink:**`)
     }
