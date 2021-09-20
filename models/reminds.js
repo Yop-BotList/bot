d09c76a8c59264dd0f2120657326df9dd92fc4ef
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-09-20 20:25:31 +0200

    Update reminds.js

--- models/reminds.js
+++ models/reminds.js
@@ -3,7 +3,6 @@ const { Schema, model } = require("mongoose"),
 rmdShema = new Schema({
     userId: { type: String, required: false },
     chanId: { type: String, required: false },
-    time: { type: String, required: false },
     endsAt: { type: String, required: false }
 }),
 
