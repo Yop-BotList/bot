19afe0b3444a3f73b8498b69258cb8919d275080
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-09-20 19:33:26 +0200

    Update inviteCreate.js

--- events/inviteCreate.js
+++ events/inviteCreate.js
@@ -1,7 +1,8 @@
-const { staffGuildId, owner } = require("../configs/config.json");
+const { staffGuildId, owner } = require("../configs/config.json"),
+    client = require("../index");
 
-module.exports = async (client, invite) => {
+client.on("inviteCreate", async (client, invite) => {
     if (invite.guild.id !== staffGuildId) return;
 
     if (invite.inviter.id !== owner) return invite.delete();
-}
+});
