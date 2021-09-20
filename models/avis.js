90f57f2dffd85ca65d26ebb1f9409050f205a5ac
Nonolanlan1007 <84439677+Nonolanlan1007@users.noreply.github.com>
2021-09-20 19:34:45 +0200

    Update avis.js

--- models/avis.js
+++ models/avis.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose"),
 
 userShema = new Schema({
     userId: { type: String, required: false },
-    avis: { type: Boolean, required: false }
+    avis: { type: String, required: false }
 }),
 
 usersShema = module.exports = model("avis", userShema);
