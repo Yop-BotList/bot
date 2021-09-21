5396e22cea5c46b27b7432de6a2ac18476a80329
Nonolanlan1007 <nono.lagraulet31@gmail.com>
2021-09-21 18:03:13 +0200

    Update inviteCreate.js

--- events/inviteCreate.js
+++ events/inviteCreate.js
@@ -1,8 +1,12 @@
-const { staffGuildId, owner } = require("../configs/config.json"),
-    client = require("../index");
+const { staffGuildId, owner, owners } = require("../configs/config.json");
 
-client.on("inviteCreate", async (client, invite) => {
+module.exports = async (client, invite) => {
     if (invite.guild.id !== staffGuildId) return;
 
     if (invite.inviter.id !== owner) return invite.delete();
-});
+    if (invite.inviter.id !== owner && !owners.includes(invite.inviter.id)) {
+        invite.delete()
+        client.users.cache.get(invite.inviter.id).send(`**${client.no} ➜ Désolé, mais vous n'avez pas l'autorisation de créer d'invitations sur ce serveur.**`)
+        return;
+    };
+}
