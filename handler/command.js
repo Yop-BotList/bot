ebfd318234631ad758627950362bdbcfe2780aa6
Nonolanlan1007 <nono.lagraulet31@gmail.com>
2021-09-21 18:49:33 +0200

    Update command.js

--- handler/command.js
+++ handler/command.js
@@ -31,7 +31,7 @@ module.exports = async (client) => {
           );
           continue;
         }
-        if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach((alias) => client.aliases.set(alias, pull.name));
+        if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach((alias) => client.aliases.set(alias, pull));
       }
     });
     console.log(table.toString().cyan);
