f2e8dfc9359e68e553b152f67291f404c065b7d2
Nonolanlan1007 <nono.lagraulet31@gmail.com>
2021-09-22 17:25:20 +0200

    Create guildMemberRemove.js

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,28 @@
+const { autokick } = require("../configs/config.json"),
+      { mainguildid } = require("../configs/config.json"),
+      { botlogs } = require("../configs/channels.json"),
+      client = require("../index"),
+      bots = require("../models/bots");
+
+client.on("guildMemberRemove", async(client, member, guild) => {
+    if (guild.id !== mainguildid) return
+    const bot = await bots.findOne({ serverID: mainguildid, ownerID: member.user.id })
+    if (bot) {
+        const robot = guild.members.cache.get(bot.botID)
+        client.channels.cache.get(client.botlogs).send({
+            embed:{
+                title: 'Auto-expultion...',
+                timestamp: new Date(),
+                thumbnail: {
+                    url: bot.user.displayAvatarURL()
+                },
+                color: '#FF0000',
+                description: `<@${member.user.id}> vient juste de quiter le serveur. Son robot <@${robot.user.id}> a donc été supprimé !`
+            }
+        });
+        setTimeout(() => {
+            if (autokick === true) robot.kick("Bot supprimé de la liste.")
+            await bots.findOneAndDelete({ serverID: mainguildid, ownerID: member.user.id })
+        }, 1000)
+    }
+})
