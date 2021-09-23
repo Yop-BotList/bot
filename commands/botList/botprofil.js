f119786cc7ccb01d01b71f5d39f9c62aec2ed45a
Nonolanlan1007 <nono.lagraulet31@gmail.com>
2021-09-23 19:06:31 +0200

    Update botprofil.js

--- commands/botList/botprofil.js
+++ commands/botList/botprofil.js
@@ -17,77 +17,66 @@ module.exports = {
      */
 
     run: async (client, message, args) => {
-        const member = message.mentions.members.first() || await bots.findOne({ serverID: mainguildid, owerID: message.author.id });
-        if (!client.dbProprio.has(`Proprio_${member.user.id}`)) return message.channel.send(client.no + " | Désolé, mais je ne retrouve pas ce bot sur ma liste (ce n'est d'ailleurs peut-être même pas un bot)")
-        let prefix = client.dbPrefix.get(`Prefix_${member.user.id}`),
-            description = client.dbDesc.get(`Desc_${member.user.id}`) || "_Aucune description définie..._",
-            support = client.dbSupport.get(`Support_${member.user.id}`) || "_Aucun serveur support défini..._",
-            siteweb = client.dbSite.get(`Site_${member.user.id}`) || "_Aucu site web défini..._";
+        const ownerdb = await bots.findOne({ serverID: mainguildid, owerID: message.author.id })
+        const member = message.mentions.members.first() || message.guild.members.cache.get(ownerdb.ownerID);
+        let db = await bots.findOne({ serverID: mainguildid, botID: member.user.id })
+        if (!db) return message.channel.send("**" + client.no + " ➜ Désolé, mais je ne retrouve pas ce bot sur ma liste (ce n'est d'ailleurs peut-être même pas un bot)**")
 
-            const votesGet = await likeshema.findOne({ botID: member.user.id, serverID: message.guild.id });
-            const votes = votesGet ? votesGet.likesCount : 0,
-                    lastlike = votesGet ? votesGet.likeDate : "*Aucun vote...*"
+            const votes = db.likesCount || 0,
+                    lastlike = db.likeDate || "*Aucun vote...*",
+                    e = new MessageEmbed()
+                    .setTitle(`Informations sur le robot ${member.user.username}`)
+                    .setColor(client.color)
+                    .setThumbnail(member.user.displayAvatarURL())
+                    .setTimestamp(new Date())
+                    .setFooter(`${member.user.username} a rejoint la liste le ${moment(member.joinedAt).format('Do MMMM YYYY')}`)
+                    .addFields(                        {
+                        name: '__:robot: Nom :__',
+                        value: `> <@${member.user.id}>`,
+                        inline: true
+                    },
+                    {
+                        name: '__:key: Propriétaire :__',
+                        value: `> <@${db.owerID}>`,
+                        inline: true
+                    },
+                    {
+                        name: '__:bookmark_tabs: Préfixe :__',
+                        value: `> ${db.prefix}`,
+                        inline: true
+                    },
+                    {
+                        name: '__:pencil: Description :__',
+                        value: `> ${db.desc}`,
+                        inline: false
+                    },
+                    {
+                        name: '__:question: Serveur support : __',
+                        value: `> ${db.serverInvite}`,
+                        inline: true
+                    },
+                    {
+                        name: '__:globe_with_meridians: Site web :__',
+                        value: `> ${db.site}`,
+                        inline: true
+                    },
+                    {
+                        name: '__:nut_and_bolt: Lien d\'invitation :__',
+                        value: `> [Clique ici](https://discord.com/oauth2/authorize?client_id=${member.user.id}&scope=bot%20application.commands&permissions=-1)`,
+                        inline: false
+                    },
+                    {
+                        name: '__:sparkling_heart: Vote(s) :__',
+                        value: `> ${votes} vote(s)`,
+                        inline: true
+                    },
+                    {
+                            name: '__:two_hearts: Dernier like :__',
+                            value: `${lastlike}`,
+                            inline: true
+                    })
             
             //Message
-            message.channel.send({
-                embed: {
-                    title: `Informations sur le bot ${member.user.username}`,
-                    color: client.color,
-                    timestamp: new Date(),
-                    thumbnail: {
-                        url: member.user.displayAvatarURL()
-                    },
-                    footer: {
-                        text: `${member.user.username} a rejoint la liste le ${moment(member.joinedAt).format('Do MMMM YYYY')}`,
-                    },
-                    fields: [
-                        {
-                            name: '__:robot: Nom :__',
-                            value: `> <@${member.id}>`,
-                            inline: true
-                        },
-                        {
-                            name: '__:key: Propriétaire :__',
-                            value: `> <@${client.dbProprio.get(`Proprio_${member.id}`)}>`,
-                            inline: true
-                        },
-                        {
-                            name: '__:bookmark_tabs: Préfixe :__',
-                            value: `> ${prefix}`,
-                            inline: true
-                        },
-                        {
-                            name: '__:pencil: Description :__',
-                            value: `> ${description}`,
-                            inline: false
-                        },
-                        {
-                            name: '__:question: Serveur support : __',
-                            value: `> ${support}`,
-                            inline: true
-                        },
-                        {
-                            name: '__:globe_with_meridians: Site web :__',
-                            value: `> ${siteweb}`,
-                            inline: true
-                        },
-                        {
-                            name: '__:nut_and_bolt: Lien d\'invitation :__',
-                            value: `> [Clique ici](https://discord.com/oauth2/authorize?client_id=${member.user.id}&scope=bot&permissions=2147483647)`,
-                            inline: false
-                        },
-                        {
-                            name: '__:sparkling_heart: Vote(s) :__',
-                            value: `> ${votes} vote(s)`,
-                            inline: true
-                        },
-                        {
-                                name: '__:two_hearts: Dernier like :__',
-                                value: `${lastlike}`,
-                                inline: true
-                        }
-                    ]
-                }
-            })
+            message.channel.send({ embeds: [e] })
     }
 }
