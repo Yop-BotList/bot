0b5351533be087ae885df6c64e31934fa7829303
Nonolanlan1007 <nono.lagraulet31@gmail.com>
2021-09-25 17:50:55 +0200

    Update suggest.js

--- commands/Utilitaires/suggest.js
+++ commands/Utilitaires/suggest.js
@@ -1,35 +1,59 @@
 const { Client, Message, MessageEmbed } = require("discord.js"),
       { bof } = require("../../configs/emojis.json"),
-      { suggests } = require("../../configs/channels.json"),
-      { prefix } = require("../../configs/config.json");
+      channels = require("../../configs/channels.json"),
+      { prefix } = require("../../configs/config.json"),
+      suggests = require("../../models/suggests"),
+      packages = require("../../package.json"),
+      bot = require("../../models/botconfig");
+
       
 module.exports = {
     name: "suggest",
-    aliases: ["sugg", "suggestion"],
-    description: "Permet d'envoyer une suggestion ,au serveur.",
+    description: "Permet d'envoyer, d'accepter, de supprimer ou de rejeter une suggestion.",
     categories: "info",
     permissions: "everyone",
     cooldown: 15,
-    usage: "suggest <suggestion>",
+    usage: "suggest <suggestion | accept | reject | delete>",
     /**
      *  @param {Client} client
      *  @param {Message} message
+     *  @param {String[]} args
      */
 
-    run: async(client, message) => {
-        if (!args[0]) return message.channel.send(`\`\`\`${prefix}suggest <suggestion>\`\`\``)
-
-        const e = new MessageEmbed()
-        .setTitle(`Nouvelle suggestion de ${message.author.username} !`)
-        .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
-        .setColor(client.color)
-        .setTimestamp(new Date())
-        .setDescription(`\`\`\`md\n# ${args.join(' ')}\n\`\`\`\n**Réagissez :**\n${client.yes} = Oui\n${bof} = Pourquoi pas ?\n${client.no} = Non`)
-        client.channels.cache.get(suggests).send({ embeds: [e] }).then(msg => {
-            msg.react('838334340618256384')
-            msg.react('838334339820945419')
-            msg.react('838334340160815104')
-            message.channel.send(`**${client.yes} ➜ Votre suggestion a bien été envoyée. Allez voir dans le <#${suggests}>.**`)
-        });
+    run: async(client, message, args) => {
+        if (!args[0]) return message.channel.send(`\`\`\`${prefix}suggest <suggestion | accept | reject | delete>\`\`\``)
+        if (args[0] !== "accept" && "reject" && "delete") {
+            const db = await bot.findOne()
+            if (!db) new bot({ suggests: 0 })
+    
+            const e = new MessageEmbed()
+            .setTitle(`Nouvelle suggestion de ${message.author.username} ! (N°${db.suggests + 1})`)
+            .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+            .setColor(client.color)
+            .setTimestamp(new Date())
+            .setDescription(`\`\`\`md\n# ${args.join(' ')}\n\`\`\`\n**Réagissez :**\n${client.yes} = Oui\n${bof} = Pourquoi pas ?\n${client.no} = Non`)
+            client.channels.cache.get(channels.suggests).send({ embeds: [e] }).then(async(msg) => {
+                msg.react('838334340618256384')
+                msg.react('838334339820945419')
+                msg.react('838334340160815104')
+    
+                // database
+                new suggests({
+                    userID: message.author.id,
+                    msgID: msg.id,
+                    suggID: db.suggests + 1,
+                    accepted: false,
+                    deleted: false,
+                    content: message.content.slice(10)
+                }).save()
+                if (db) await bot.findOneAndUpdate({ suggests: db.suggests + 1 })
+                
+    
+                return message.channel.send(`**${client.yes} ➜ Votre suggestion a bien été envoyée. Allez voir dans le <#${channels.suggests}>.**`)
+            });
+        }
+        if (args[0] === "accept") {
+            if (!message.member.permissions.has("ADMINISTRATOR")) return message.channel.send(`**${client.no} ➜ Vous n'avez pas la permission d'utiliser cet argument.`)
+        }
     }
 }
