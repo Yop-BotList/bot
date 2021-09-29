0ebe2f045b8452474d1c396dad7bb1434f3f9890
Nonolanlan1007 <nolhan.dev@gmail.com>
2021-09-29 18:38:11 +0200

    Update warn.js

--- commands/STAFF/warn.js
+++ commands/STAFF/warn.js
@@ -1,6 +1,7 @@
 const { Client, Message, MessageEmbed } = require('discord.js'),
       warns = require("../../models/sanction"),
-      { modlogs } = require("../../configs/channels.json");
+      { modlogs } = require("../../configs/channels.json"),
+      bot = require("../../models/botconfig")
 
 module.exports = {
     name: 'warn',
@@ -17,11 +18,39 @@ module.exports = {
      */
     run: async(client, message, args) => {
         const member = client.users.cache.get(args[0]);
-        if (!member) return message.reply(`**${client.no} ➜ Veuillez entrer l'identifiant d'un membre présent sur ce serveur.**`)
+        if (!member) return message.reply(`**${client.no} ➜ Veuillez entrer un identifiant valide.**`)
         if (!args[1]) return message.reply(`**${client.no} ➜ Veuillez entrer une raison.**`)
+       const db = bot.findOne()
         
         new warns({
-            userID: member.username.id
+            userID: member.id,
+            modID: message.author.id,
+            wrnID: db.warns + 1,
+            reason: args.slice(1).join(" "),
+            type: "WARN"
         }).save()
+        
+        const e = new MessageEmbed()
+        .setTitle("Nouvelle sanction :")
+        .setThumbnail(member.displayAvatarURL({ dynamic: true }))
+        .setColor(client.color)
+        .setTimestamp(new Date())
+        .addField(`:busts_in_silhouette: ➜ Utilisateur :`, `\`\`\`md\n# ${member.tag} ➜ ${member.id}\`\`\``)
+        .addField(`:dividers: ➜ Type :`, `\`\`\`md\n# WARN\`\`\``)
+        .addField(`:newspaper2: ➜ Raison(s) :`, `\`\`\`md\n# ${args.slice(1).join(" ")}\`\`\``)
+        .addField(`:man_police_officer: ➜ Modérateur :`, `\`\`\`md\n# ${message.author.tag} ➜ ${message.author.id}\`\`\``)
+        .addField(`:1234: Code`, `\`\`\`md\n# ${db.warns + 1}\`\`\``)
+        const e2 = new MessageEmbed()
+        .setTitle("Nouvelle sanction :")
+        .setThumbnail(member.displayAvatarURL({ dynamic: true }))
+        .setColor(client.color)
+        .setTimestamp(new Date())
+        .addField(`:dividers: ➜ Type :`, `\`\`\`md\n# WARN\`\`\``)
+        .addField(`:newspaper2: ➜ Raison(s) :`, `\`\`\`md\n# ${args.slice(1).join(" ")}\`\`\``)
+        member.send({ embeds: [e2] }).catch(() => {
+            e.addField(":warning: Avertissement :", "L'utilisateur n'a pas été prévenu(e) de sa santion !")
+        })
+        client.channels.cache.get(modlogs).send({ embeds: [e] })
+        message.reply(`**${client.yes} ➜ ${member.tag} a été averti avec succès !**`)
     }
 }
