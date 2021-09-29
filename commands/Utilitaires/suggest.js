6a4e9e80d063a745df969319663bab4f74ba3b18
Nonolanlan1007 <nolhan.dev@gmail.com>
2021-09-29 17:14:00 +0200

    Update suggest.js

--- commands/Utilitaires/suggest.js
+++ commands/Utilitaires/suggest.js
@@ -21,10 +21,13 @@ module.exports = {
      */
 
     run: async(client, message, args) => {
+
+        // create
         if (!args[0]) return message.channel.send(`\`\`\`${prefix}suggest <suggestion | accept | reject | delete | list>\`\`\``)
-        if (args[0] !== "accept" && "reject" && "delete" && "list") {
+        if (args[0] !== "accept" && args[0] !== "reject" && args[0] !== "delete") {
+            const db1 = await bot.findOne()
+            if (!db1) new bot({ suggests: 0 }).save()
             const db = await bot.findOne()
-            if (!db) new bot({ suggests: 0 })
     
             const e = new MessageEmbed()
             .setTitle(`Nouvelle suggestion de ${message.author.username} ! (N°${db.suggests + 1})`)
@@ -44,7 +47,7 @@ module.exports = {
                     suggID: db.suggests + 1,
                     accepted: false,
                     deleted: false,
-                    content: message.content.slice(10)
+                    content: args.join(" ")
                 }).save()
                 if (db) await bot.findOneAndUpdate({ suggests: db.suggests + 1 })
                 
@@ -52,13 +55,71 @@ module.exports = {
                 return message.channel.send(`**${client.yes} ➜ Votre suggestion a bien été envoyée. Allez voir dans le <#${channels.suggests}>.**`)
             });
         }
+
+        // accept
         if (args[0] === "accept") {
             if (!message.member.permissions.has("ADMINISTRATOR")) return message.channel.send(`**${client.no} ➜ Vous n'avez pas la permission d'utiliser cet argument.**`)
             if (!args[1]) return message.channel.send(`**${client.no} ➜ Veuillez entrer un identifiant de suggestion.**`)
-            const db = await bot.findOne({ suggestID: parseInt(args[1]), accepted: false, deleted: false });
+            const db = await suggests.findOne({ suggestID: args[1], accepted: false, deleted: false });
             if (!db) return message.channel.send(`**${client.no} ➜ Veuillez entrer un identifiant de suggestion valide ou sur laquelle aucune action n'a été effectuée.**`)
-            const msg = await message.guild.channels.cache.get(channels.suggests).messages.fetch(db.messageID);
-            msg.edit({ content: "yo" })
+            const member = message.guild.members.cache.get(db.userID),
+                  reason = args.slice(2).join(" ") || `Aucun commentaire...`
+
+            message.guild.channels.cache.get(channels.suggests).messages.fetch(db.msgID).then(async (msg) => {
+                const e1 = new MessageEmbed()
+                .setTitle(`Suggestion de ${member.user.username} acceptée par ${message.author.username} !`)
+                .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+                .setColor(client.color)
+                .setTimestamp(new Date())
+                .setDescription(`\`\`\`md\n# ${db.content}\n\`\`\``)
+                .addField(`Commentaire de ${message.author.username} :`, `\`\`\`diff\n+ ${reason}\n\`\`\``)
+
+                msg.edit({ embeds: [e1] });
+
+                await suggests.findOneAndUpdate({ suggID: db.suggID }, { $set: { accepted: true } }, { upsert: true });
+
+                const e2 = new MessageEmbed()
+                .setColor(client.color)
+                .setDescription(`<@${message.author.id}> vient tout juste d'accepter votre [suggestion](https://discord.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id}) ! Merci à toi !`)
+
+                message.guild.channels.cache.get(channels.sugglogs).send({ content: `<@${db.userID}>`, embeds: [e2] })
+
+                message.channel.send(`**${client.yes} ➜ La suggestion a bien été acceptée !**`)
+            })
+        }
+
+        // reject
+        if (args[0] === "reject") {
+            if (!message.member.permissions.has("ADMINISTRATOR")) return message.channel.send(`**${client.no} ➜ Vous n'avez pas la permission d'utiliser cet argument.**`)
+            if (!args[1]) return message.channel.send(`**${client.no} ➜ Veuillez entrer un identifiant de suggestion.**`)
+            const db = await suggests.findOne({ suggestID: args[1], accepted: false, deleted: false });
+            if (!db) return message.channel.send(`**${client.no} ➜ Veuillez entrer un identifiant de suggestion valide ou sur laquelle aucune action n'a été effectuée.**`)
+            const member = message.guild.members.cache.get(db.userID),
+                  reason = args.slice(2).join(" ")
+
+            if (!reason) return message.channel.send(`**${client.no} ➜ Veuillez entrer une raison.**`)
+
+            message.guild.channels.cache.get(channels.suggests).messages.fetch(db.msgID).then(async (msg) => {
+                const e1 = new MessageEmbed()
+                .setTitle(`Suggestion de ${member.user.username} refusée par ${message.author.username} !`)
+                .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+                .setColor(client.color)
+                .setTimestamp(new Date())
+                .setDescription(`\`\`\`md\n# ${db.content}\n\`\`\``)
+                .addField(`Commentaire de ${message.author.username} :`, `\`\`\`diff\n- ${reason}\n\`\`\``)
+
+                msg.edit({ embeds: [e1] });
+
+                await suggests.findOneAndUpdate({ suggID: db.suggID }, { $set: { accepted: true } }, { upsert: true });
+
+                const e2 = new MessageEmbed()
+                .setColor(client.color)
+                .setDescription(`<@${message.author.id}> vient tout juste de refuser votre [suggestion](https://discord.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id}) ! Merci quand même d'avoir proposé quelque chose !`)
+
+                message.guild.channels.cache.get(channels.sugglogs).send({ content: `<@${db.userID}>`, embeds: [e2] })
+
+                message.channel.send(`**${client.yes} ➜ La suggestion a bien été refusée !**`)
+            })
         }
     }
 }
