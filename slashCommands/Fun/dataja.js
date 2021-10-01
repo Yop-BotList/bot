2a877bfeaadba9d55108ee0e3f74cfcc16d324f4
Nonolanlan1007 <nolhan.dev@gmail.com>
2021-10-01 18:44:52 +0200

    Update dataja.js

--- slashCommands/Fun/dataja.js
+++ slashCommands/Fun/dataja.js
@@ -1,1 +1,14 @@
+const { ContextMenuInteraction, MessageEmbed } = require('discord.js');
 
+module.exports = {
+    name: 'dataja',
+    description: 'Don’t ask to ask, juste ask !',
+
+    /** 
+     * @param {Client} client 
+     * @param {ContextMenuInteraction} interaction
+     */
+    run: async(client, interaction) => {
+         message.reply('"Ne demande pas si tu peux demander, mais demande directement. Ça nous fait gagner du temps."\n*Source : <https://dontasktoask.com/>*')
+    },
+};
