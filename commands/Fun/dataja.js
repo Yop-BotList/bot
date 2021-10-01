de76a681946be39edd628dbdae945b16d1773359
Nonolanlan1007 <nolhan.dev@gmail.com>
2021-10-01 18:41:01 +0200

    Create dataja.js

---
+++ commands/Fun/dataja.js
@@ -0,0 +1,20 @@
+const { Client, Message, MessageEmbed } = requier("discord.js");
+
+module.exports = {
+    name: "dataja",
+    categories: "fun",
+    permissions: "everyone",
+    description: "Ne demandez pas si vous pouvez demander, mais demandez directement !",
+    aliases: [],
+    usage: "dataja",
+    
+    /**
+    * @param {Message} message
+    * @param {Client} client
+    * @param {String[]} args
+    */
+    run: async (client, message, args) => { 
+       message.reply('"Ne demande pas si tu peux demander, mais demande directement. Ça nous fait gagner du temps."\n*Source : <https://dontasktoask.com/>*')
+       message.delete()
+    }
+}
