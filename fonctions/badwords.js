38b861982b6a5cac6fdd97083cf7578989acfa0c
Nolhan#2508 <nolhan.dev@gmail.com>
2021-10-02 18:50:08 +0200

    Update badwords.js

--- fonctions/badwords.js
+++ fonctions/badwords.js
@@ -4,5 +4,21 @@ const { bypass } = require("../configs/roles.json"),
 
 badWords = module.exports = async (message) => {
     if (message.member.roles.has(bypass)) return;
+    let text = message.content;
     
+    // French 
+    badwords.french.forEach(x => {
+        let size = x.length;
+        text = text.toLowerCase();
+        text = text.replace(x, "\*".repeat(size)).then(() => { const content = true });
+    });
+
+    // English 
+    badwords.english.forEach(x => {
+        let size = x.length;
+        text = text.toLowerCase();
+        text = text.replace(x, "\*".repeat(size)).then(() => { const content = true });
+    });
+
+   
 }
