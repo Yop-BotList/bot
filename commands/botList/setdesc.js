eb7d67280a461a4977506ea2db600c4596edd8a4
Nonolanlan1007 <nolhan.dev@gmail.com>
2021-10-06 15:30:37 +0200

    Create setdesc.js

---
+++ commands/botList/setdesc.js
@@ -0,0 +1,77 @@
+const { Client, Message, MessageEmbed } = require('discord.js'),
+      bots = require("../../models/bots"),
+      { prefix } = require("../../configs/config.json"),
+      { botslogs } = require('../../configs/channels.json'),
+      { verificator } = require("../../configs/roles.json");
+
+module.exports = {
+    name: 'setdesc',
+    aliases: ['botdesc'],
+    categories : 'botlist', 
+    permissions : 'everyone', 
+    description: 'Modifier la description d\'un robot.',
+    cooldown : 5,
+    usage: 'setdesc <id> <description | none>',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async(client, message, args) => {
+        if (!args[0]) return message.channel.send(`\`\`\`${prefix}setdesc <id bot> <description | none>\`\`\``)
+        const member = message.guild.members.cache.get(`${args[0]}`);
+        if (!member) return message.channel.send(`**${client.no} ➜ Veuillez entrer l'indentifiant valide d'un bot présent sur ce serveur.**`)
+        const db = await bots.findOne({ botID: member.user.id });
+        if (!db) return message.channel.send("**" + client.no + ' ➜ Désolé, mais je ne retrouve pas ce bot sur ma liste. (Ce n\'est d\'ailleurs peut-être même un bot)**')
+        if (db.ownerID !== message.author.id && !message.member.roles.cache.get(verificator)) return message.channel.send("**" + client.no + " ➜ Désolé, mais vous n'avez pas la permission d'utiliser cette commande.**")
+        if (!args[1]) return message.channel.send("**" + client.no + ' ➜ Il faudrai peut-être entrer une description non ?**')
+        if (args[1] === 'none' && db.desc) {
+            const e = new MessageEmbed()
+            .setColor(client.color)
+            .setTitle("Modification du profil...")
+            .setThumbnail(member.user.displayAvatarURL())
+            .setTimestamp(new Date())
+            .setDescription(`<@${message.author.id}> vient juste d'éditer la description de votre robot <@${member.id}> :`)
+            .setFields({
+                name: "➜ Avant :",
+                value: `\`\`\`${db.desc}\`\`\``,
+                inline: false
+            },
+            {
+                name: "➜ Après :",
+                value: `\`\`\`none\`\`\``,
+                inline: false
+            })
+            client.channels.cache.get(botslogs).send({ content: `<@${db.ownerID}>`, embeds: [e] })
+            message.channel.send("**" + client.yes + " ➜ Modifications enregistrées avec succès !**")
+            setTimeout(async () => {
+                return await bots.findOneAndUpdate({ botID: member.user.id }, { $set: { desc: null } }, { upsert: true })
+            }, 2000)
+        }
+        if (args[1] === 'none' && !db.desc) return message.channel.send("**" + client.no + ' ➜ Tu m\'as demandé supprimer une description qui n\'a jamais été enregistrée ¯\\_(ツ)_/¯**')
+        if (args[1] !== "none") {
+            if (message.content.length > 300) return message.channel.send("**" + client.no + " ➜ Votre description ne doit pas dépasser les 300 caractères.**")
+            const e = new MessageEmbed()
+            .setColor(client.color)
+            .setTitle("Modification du profil...")
+            .setThumbnail(member.user.displayAvatarURL())
+            .setTimestamp(new Date())
+            .setDescription(`<@${message.author.id}> vient juste d'éditer la description de votre robot <@${member.id}> :`)
+            .setFields({
+                name: "➜ Avant :",
+                value: `\`\`\`none\`\`\``,
+                inline: false
+            },
+            {
+                name: "➜ Après :",
+                value: `\`\`\`${args.slice(1).join(" ")}\`\`\``,
+                inline: false
+            })
+            client.channels.cache.get(botslogs).send({ content: `<@${db.ownerID}>`, embeds: [e] })
+            message.channel.send("**" + client.yes + " ➜ Modifications enregistrées avec succès !**")
+            setTimeout(async () => {
+                return await bots.findOneAndUpdate({ botID: member.user.id }, { $set: { desc: args.slice(1).join(" ") } }, { upsert: true })
+            }, 2000)
+        }
+    }
+}
