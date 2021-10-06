30d2e0950fdcec32448315e4acfdbabe593aeadf
Nonolanlan1007 <nolhan.dev@gmail.com>
2021-10-06 14:57:09 +0200

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,29 +1,26 @@
+const { MessageEmbed } = require("discord.js");
 const { autokick } = require("../configs/config.json"),
       { mainguildid } = require("../configs/config.json"),
       { botlogs } = require("../configs/channels.json"),
       client = require("../index"),
       bots = require("../models/bots");
 
-client.on("guildMemberRemove", async (client, member, guild) => {
-    if (guild.id !== mainguildid) return
-    const botget = await bots.find({ serverID: mainguildid, ownerID: member.user.id })
+client.on("guildMemberRemove", async (client, member) => {
+    if (member.guild.id !== mainguildid) return
+    const botget = await bots.find({ ownerID: member.user.id })
     if (botget) {
         botget.forEach(async x => {
-            const robot = guild.members.cache.get(x.botID)
-            client.channels.cache.get(botlogs).send({
-                embed:{
-                    title: 'Auto-expultion...',
-                    timestamp: new Date(),
-                    thumbnail: {
-                        url: robot.user.displayAvatarURL()
-                    },
-                    color: '#FF0000',
-                    description: `<@${member.user.id}> vient juste de quiter le serveur. Son robot <@${robot.user.id}> a donc été supprimé !`
-                }
-            });
+            const robot = member.guild.members.cache.get(x.botID),
+                  e = new MessageEmbed()
+                  .setTitle("Auto-expultion...")
+                  .setColor(client.color)
+                  .setTimestamp(new Date())
+                  .setThumbnail(robot.user.displayAvatarURL())
+                  .setDescription(`<@${member.user.id}> vient juste de quiter le serveur. Son robot <@${robot.user.id}> a donc été supprimé !`)
+            client.channels.cache.get(botlogs).send({ embeds: [e] });
 
             if (autokick === true) robot.kick("Bot supprimé de la liste.")
-            await bots.deleteOne({ serverID: mainguildid, botID: x.botID })
+            await bots.deleteOne({ botID: x.botID })
         });
     }
 })
