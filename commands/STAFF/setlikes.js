771ab8bb5da5ab8a51b968c0a659c7fd2fc03039
Nonolanlan1007 <nolhan.dev@gmail.com>
2021-10-11 19:11:49 +0200

    Update setlikes.js

--- commands/STAFF/setlikes.js
+++ commands/STAFF/setlikes.js
@@ -1,7 +1,8 @@
 const { verificator } = require("../../configs/roles.json"),
     { Client, Message } = require("discord.js"),
     bots = require("../../models/bots"),
-    moment = require("moment");
+    moment = require("moment"),
+    { mainguildid } = require("../../configs/config.json");
 
     moment.locale("fr")
 
@@ -11,7 +12,7 @@ module.exports = {
     permissions : verificator, 
     description: "Permet de rénitialiser ou changer le nombre de likes d'un bot.",
     cooldown : 3600,
-    usage: 'setlikes <set/reset> <bot> <number>',
+    usage: 'setlikes <set/reset> <utilisateur | all> [number]',
 
     /**
      * @param {Client} client
@@ -38,13 +39,13 @@ module.exports = {
                 botID: bot.user.id
             }, {
                 likesCount: amount,
-                likeDate: `Le ${moment().format("Do MMMM YYYY")} à ${moment().format("HH")}h${moment().format("mm")}`
+                likeDate: `Le ${moment().format("Do MMMM YYYY")} à ${moment().format("HH")}h${moment().format("mm")} par un administrateur.`
             }, {
                 upsert: true
             });
 
             message.reply({ content: `${client.yes} ➜ Le nombre de likes de \`${bot.user.tag}\` est maintenant défini sur ${amount}.` });
-        } else if (args[0] === "reset") {
+        } else if (args[0] === "reset" && args[1] !== "all") {
             let bot = message.mentions.members.first();
             if (!bot) return message.reply({ content: `${client.no} ➜ Merci de mentionner un bot.` });
             if (!bot.user || !bot.user.bot) return message.reply({ content: `${client.no} ➜ ${args[1]} n'est pas un bot.` });
@@ -62,7 +63,28 @@ module.exports = {
                 upsert: true
             });
 
-            message.reply({ content: `${client.yes} ➜ Le nombre de votes de \`${bot.user.tag}\` vient juste d'être réinitialisé.` });
+            return message.reply({ content: `${client.yes} ➜ Le nombre de votes de \`${bot.user.tag}\` vient juste d'être réinitialisé.` });
+        } else if (args[0] === "reset" && args[1] === "all") {
+            const m = await message.reply(`**${client.yes} ➜ Approuvez-vous cette action ? Cela signifie que tous les votes vont être réinitialisés.**`)
+            m.react("✅")
+            m.react("❌")
+            const filter = (reaction, user) => user.id === message.author.id;
+            const collector = m.createReactionCollector({ filter, time: 11000000, max: 1, })
+            collector.on('collect', async r => {
+                if (r.emoji.name === "✅") {
+                    const newchannel = await bots.find({ serverID: mainguildid });
+                    newchannel.forEach(async l => {
+                        const dell = await bots.findOneAndUpdate({ serverID: mainguildid, _id: l._id }, { $set: { likesCount: null, likeDate: null } }, { upsert: true });
+                    })
+                    return m.edit(`**${client.yes} ➜ Tous les votes ont bien été réinitialisés !**`)
+                }
+                if (r.emoji.name === "❌") {
+                    m.edit(`**${client.yes} ➜ Action annulée avec succès !**`);
+                }
+            });
+            collector.on('end', async collected => {
+                m.reactions.removeAll()
+            });
         } else return message.reply({ content: `${client.no} ➜ Merci de mettre une des deux options, \`set/reset\`.` });
     }
 }
