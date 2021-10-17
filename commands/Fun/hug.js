5d832cde03eae99efd374e73d989b38188612a7a
Nolhan#2508 <nolhan.dev@gmail.com>
2021-10-17 19:38:34 +0200

    Update hug.js

--- commands/Fun/hug.js
+++ commands/Fun/hug.js
@@ -18,11 +18,10 @@ class Hug extends Command {
     async run(client, message, args) {
         const member = message.mentions.members.first();
         if (!member) return message.reply(`**${client.no} ➜ À qui souhaites-tu faire un câlin ?**`)
-        const hug = ["https://tenor.com/view/anime-cute-hug-gif-14577424", "https://tenor.com/view/tackle-hug-couple-anime-cute-couple-love-gif-17023255", "https://tenor.com/view/hug-k-on-anime-cuddle-gif-16095203", "https://tenor.com/view/hug-anime-gif-19674705", "https://tenor.com/view/teria-wang-kishuku-gakkou-no-juliet-hug-anime-gif-16509980", "https://tenor.com/view/hug-anime-love-sweet-tight-hug-gif-7324587", "https://tenor.com/view/anime-choke-hug-too-tight-gif-14108949"],
-              e = new MessageEmbed()
-        .setTitle(`${message.author.id} fait un câlin à ${member.user.username}`)
+        const e = new MessageEmbed()
+        .setTitle(`<@${message.author.id}> fait un câlin à ${member.user.username}`)
         .setTimestamp(new Date())
-        .setImage(hug[Math.floor(Math.random() * hug.length)]) 
+        .setImage("https://i.imgur.com/TuMgJw0.gif") 
         .setColor(client.color)
         
         message.reply({ content: `<@${member.user.id}>`, embeds: [e] });
