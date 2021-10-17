f955eea0b5d8045340712e45ce874f8703072c09
Nolhan#2508 <nolhan.dev@gmail.com>
2021-10-17 19:41:58 +0200

    Update kiss.js

--- commands/Fun/kiss.js
+++ commands/Fun/kiss.js
@@ -18,11 +18,10 @@ class Kiss extends Command {
     async run(client, message, args) {
         const member = message.mentions.members.first();
         if (!member) return message.reply(`**${client.no} ➜ Qui souhaites-tu embrasser ?**`)
-        const kiss = ["https://tenor.com/view/love-cheek-peck-kiss-anime-gif-17382412", "https://tenor.com/view/toloveru-unexpected-surprise-kiss-gif-5372258", "https://tenor.com/view/kiss-anime-shocked-blush-faint-gif-16477767", "https://tenor.com/view/love-anime-kiss-hot-damn-gif-9838409", "https://tenor.com/view/yes-love-couple-kiss-in-love-gif-15009390", "https://tenor.com/view/anime-kissing-kiss-love-gif-10356314", "https://tenor.com/view/anime-kiss-gif-13221050"],
-              e = new MessageEmbed()
-        .setTitle(`${message.author.username} embrasse ${member.user.username}`)
+        const e = new MessageEmbed()
+        .setTitle(`<@${message.author.id}> embrasse ${member.user.username}`)
         .setTimestamp(new Date())
-        .setImage(kiss[Math.floor(Math.random() * kiss.length)])
+        .setImage("https://i.imgur.com/GTeWgtB.gif")
         .setColor(client.color)
         
         message.reply({ content: `<@${member.user.id}>`, embeds: [e] }); MessageEmbed
