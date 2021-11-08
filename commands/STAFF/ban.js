7b6cdc29e77ece5b228ad212b4b2cb0aaecd8237
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-11-08 20:20:46 +0100

    Update ban.js

--- commands/STAFF/ban.js
+++ commands/STAFF/ban.js
@@ -23,12 +23,13 @@ class Ban extends Command {
     }
 
     async run(client, message, args) {
+        if (!args[0]) return message.reply({ content: `**${client.no} ➜ Vous n'avez pas donné un identifiant d'utilisateur.**" });
         const member = await message.guild.members.fetch(args[0]);
         if (!member) return message.reply(`**${client.no} ➜ Veuillez entrer un identifiant valide.**`)
         if (member.user.bot) return message.reply(`**${client.no} ➜ Ce membre n’est pas humain.**`)
-        if (member.roles.highest.position >= message.member.roles.highest.position) return message.reply(`**${client.no} ➜ Ce membre est au même rang ou plus haut que vous dans la hiérarchie des rôles de ce serveur. Vous ne pouvez donc pas le sanctionner.**`)
-        if (member.roles.cache.has(bypass)) return message.reply(`**${client.no} ➜ Ce membre est imunisé contre les sanctions.**`)
-        if (!member.bannable) return message.reply(`**${client.no} ➜ Zut alors ! Je ne peux pas bannir ce membre ! Essaie peut-être de mettre mon rôle un peu plus haut dans la hiérarchie du serveur :p**`)
+        if (member.roles?.highest.position >= message.member.roles?.highest.position) return message.reply(`**${client.no} ➜ Ce membre est au même rang ou plus haut que vous dans la hiérarchie des rôles de ce serveur. Vous ne pouvez donc pas le sanctionner.**`)
+        if (member.roles?.cache.has(bypass)) return message.reply(`**${client.no} ➜ Ce membre est imunisé contre les sanctions.**`)
+        if (!member?.bannable) return message.reply(`**${client.no} ➜ Zut alors ! Je ne peux pas bannir ce membre ! Essaie peut-être de mettre mon rôle un peu plus haut dans la hiérarchie du serveur :p**`)
         if (!args[1]) return message.reply(`**${client.no} ➜ Veuillez entrer une raison.**`)
         const db = await botconfig.findOne()
         
@@ -62,8 +63,8 @@ class Ban extends Command {
         member.user.send({ embeds: [e2] }).catch(() => {
             e.addField(":warning: Avertissement :", "L'utilisateur n'a pas été prévenu(e) de sa santion !")
         })
-        member.ban({ reason: args.slice(1).join(" ") })
-        client.channels.cache.get(modlogs).send({ embeds: [e] })
+        member?.ban({ reason: args.slice(1).join(" ") })
+        client.channels?.cache.get(modlogs)?.send({ embeds: [e] })
         message.reply(`**${client.yes} ➜ ${member.user.tag} a été banni avec succès !**`)
     }
 }
