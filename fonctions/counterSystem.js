0646c34af1a9394c079ce65916ea8a5aa21283e0
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-12-02 17:15:56 +0100

    Update counterSystem.js

--- fonctions/counterSystem.js
+++ fonctions/counterSystem.js
@@ -4,24 +4,34 @@ const counter = require("../models/counter"),
 module.exports = async (message) => {
     const { channelId, content, author } = message;
     if (message.channelId !== counterChannel) return;
+    if (author.bot) return;
     
     const number = parseInt(content),
           get = await counter.findOne();
     
-    if (get.lastUser === author.id) return await message.reply({
-        content: "Tu ne peux pas envoyer deux nombres à la suite.",
-        ephemeral: true
-    });
+    if (get.lastUser === author.id) { 
+        await message.reply({
+            content: "Tu ne peux pas envoyer deux nombres à la suite.",
+            ephemeral: true
+        });
+        return message?.delete();
+    }
     
-    if (!isNaN(number)) return await message.reply({
-        content: "Tu n'as pas envoyé de nombre ou ton message contient du texte.",
-        ephemeral: true
-    });
+    if (!isNaN(number)) {
+        await message.reply({
+            content: "Tu n'as pas envoyé de nombre ou ton message contient du texte.",
+            ephemeral: true
+        });
+        return message?.delete();
+    }
     
-    if ((get.lastNumber + 1) !== number)) return await message.reply({
-        content: `Le nombre suivant est ${get.lastNumber + 1}, il ne peut pas être un autre.`,
-        ephemeral: true
-    });
+    if ((get.lastNumber + 1) !== number)) {
+        await message.reply({
+            content: `Le nombre suivant est ${get.lastNumber + 1}, il ne peut pas être un autre.`,
+            ephemeral: true
+        });
+        return message?.delete();
+    }
     
     await counter.findOneAndUpdate({
         lastNumber: get.lastNumber
