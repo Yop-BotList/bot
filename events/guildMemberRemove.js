ca5997db9b9cc9de5b78813dd9f59df5185ae3d0
Nolhan#2508 <nolhan.dev@gmail.com>
2021-12-25 09:30:03 +0100

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,7 +3,7 @@
 const { MessageEmbed } = require("discord.js"),
       { autokick } = require("../configs/config.json"),
       { mainguildid } = require("../configs/config.json"),
-      { botlogs } = require("../configs/channels.json"),
+      { botslogs } = require("../configs/channels.json"),
       bots = require("../models/bots");
 
 module.exports = async(client, member) => {
@@ -18,7 +18,7 @@ module.exports = async(client, member) => {
                   .setTimestamp(new Date())
                   .setThumbnail(robot.user.displayAvatarURL())
                   .setDescription(`<@${member.user.id}> vient juste de quiter le serveur. Son robot <@${robot.user.id}> a donc été supprimé !`)
-            client.channels.cache.get(botlogs).send({ embeds: [e] });
+            client.channels.cache.get(botslogs).send({ embeds: [e] });
 
             if (autokick === true) robot.kick("Bot supprimé de la liste.")
             await bots.deleteOne({ botID: x.botID })
