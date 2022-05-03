19d3b0adba0d660e4279c73d389e2497dcbc572b
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2022-05-03 20:46:48 +0200

    Update config.ts

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,7 +1,7 @@
 export default {
-    token: "Nzg4MDg0OTk4ODAwMjc3NTY0.X9eXWQ.rqunfxMPobFlN8ZVhjKErL4ncTg",
+    token: "token",
     prefix: "y!",
-    mongooseConnectionString: "mongodb+srv://Valredstone:wwLNg1DPYGqPymCO@cluster0.wiyny.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    mongooseConnectionString: "mongoConnectionString",
     color: "#f2ac34",
     autokick: true,
     staffGuildId: "784122752315555860",
