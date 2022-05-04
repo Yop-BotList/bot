eb3bed72eec56c7295a2269786eb88824cf8346c
Nonolanlan1007 <nolhan.dev@gmail.com>
2022-05-04 14:59:08 +0200

    Ajout de tous les emojis

--- src/configs/emotes.ts
+++ src/configs/emotes.ts
@@ -1,6 +1,6 @@
 export default {
-    yes: "<a:oui:900773800973565972>",
-    no: "<a:non:900773800193437697>",
+    yes: "<a:oui2:909015463265173565>",
+    no: "<a:non2:909015461805580298>",
     bof: "<a:bof:900773799035818024>",
     offline: "<a:offline:900773807541858304>",
     online: "<a:online:900773806124187698>",
@@ -11,5 +11,41 @@ export default {
     loading: "<a:loading:900786089508347924>",
     sort: "<a:sort:900773831424217148>",
     entre: "<a:entre:900773833001304115>",
-    alerte: "<a:alerte:900773821156573245>"
+    alerte: "<a:alerte:900773821156573245>",
+    notif: "<a:notifs:903926942439473203>",
+    question: "<a:question:933275771521359903>",
+    cadena: "<a:cadena:935804916348354620>",
+
+    badges: {
+        verifieddevelopper: "<:verified_developper:907702152166785034>",
+        balance: "<:balance_hype_squad:907702154079375430>",
+        mod: "<:Discord_moderator:907702154649796688>",
+        bravery: "<:bravery_hype_squad:907702156465958972>",
+        bughuntergold: "<:bug_hunter_gold:907702156944097371>",
+        bughunter: "<:bug_hunter:907702156717592727>",
+        brillance: "<:brillance_hype_squad:907702156944097372>",
+        hypesquadevent: "<:hypesquad_event:907702157099286550>",
+        partner: "<:partner:907718760394268673>",
+        staff: "<:discord_staff:907718915508019251>",
+        earlysupporter: "<:early_supporter:907719041773355068>",
+        verifiedbot: "<:verified_bot:907997299962163251>",
+        system: "<:system:908000945475031120>",
+        badges: "<a:badges:907996994595848195>"
+    },
+    discordicons: {
+        list: "<:list:907877680395993110>",
+        bot: "<:bot:907884774843117618>",
+        textchannel: "<:channel_text:907884996692426752>",
+        wave: "<:wave:907885147456679966>",
+        entre: "<:entre:907996992154787881>",
+        game: "<:game:907996992737779773>",
+        id: "<:id:909065946084311061>",
+        hierarchie: "<:hierarchie:907996994595848192>",
+        key: "<:key:907996994344206439>",
+        man: "<:man:907996994344206440>",
+        img: "<:image:907996994784600064>",
+        tag: "<:tag:907996994943995924>",
+        clyde: "<:clyde:909065946050723930>",
+        horloge: "<:horloge:949578037509382166>"
+}
 }
