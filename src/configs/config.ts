90e628926d6c7e3e04ad94577aedd5bc9f4d119d
Nolhan#2508 <nolhan.dev@gmail.com>
2022-10-07 17:48:45 +0200

    Update

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,18 +1,15 @@
 export default {
-    token: "Nzg4MDg0OTk4ODAwMjc3NTY0.GXixHw.73YA-EmLW-cDrYF_rUFIbXiIKIz55jP5iByRAc",
-    prefix: "y/",
-    mongooseConnectionString: "mongodb+srv://DBTestUser:muLnesNRPeldn15f@cluster0.pqoag.mongodb.net/?retryWrites=true&w=majority",
+    token: "token",
+    prefix: "y!",
+    mongooseConnectionString: "url",
     color: {
         hexa: "#f2ac34",
         integer: 0xf2ac34
     },
     autokick: true,
-    staffGuildId: "839895687340753026",
-    owners: [
-        "692374264476860507",
-        "650664078649458699"
-    ],
-    mainguildid: "839895687340753026",
+    staffGuildId: "",
+    owners: [],
+    mainguildid: "",
     antiinvite: true,
     citations: true
 };
