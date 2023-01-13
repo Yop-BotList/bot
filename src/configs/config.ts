1d337c4daebf6a5588be3876f579697b9d6a78c5
Kwinty <joakim1520@gmail.com>
2023-01-13 15:13:25 +0100

    test

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,30 +1,30 @@
 export default {
-    token: "",
-    prefix: "y/",
-    mongooseConnectionString: "",
-    color: {
-        hexa: "#f2ac34",
-        integer: 0xf2ac34
-    },
-    autokick: true,
-    staffGuildId: "839895687340753026",
-    owners: [
-        "692374264476860507",
-        "650664078649458699",
-        "458218450393890831"
-    ],
-    mainguildid: "839895687340753026",
-    antiinvite: true,
-    citations: true,
-    automod: {
-        ignoreBots: true,
-        ignoreStaff: true,
-        maxMessages: {
-            antispam: 10,
-            antilink: 10,
-            antiinvites: 5,
-            massmentions: 5
-        },
-        badwords: ["con", "connard", "pute", "fdp", "geule", "connasse", "merde", "chier", "chiant", "chiante", "nique", "niquer", "salop", "salope", "salopard", "ntm"]
-    }
+  token: "TOKEN",
+  prefix: "y/",
+  mongooseConnectionString: "URL MONGO",
+  color: {
+      hexa: "#f2ac34",
+      integer: 0xf2ac34
+  },
+  autokick: true,
+  staffGuildId: "839895687340753026",
+  owners: [
+      "692374264476860507",
+      "650664078649458699",
+      "458218450393890831"
+  ],
+  mainguildid: "839895687340753026",
+  antiinvite: true,
+  citations: true,
+  automod: {
+      ignoreBots: true,
+      ignoreStaff: true,
+      maxMessages: {
+          antispam: 10,
+          antilink: 10,
+          antiinvites: 5,
+          massmentions: 5
+      },
+      badwords: ["con", "connard", "pute", "fdp", "geule", "connasse", "merde", "chier", "chiant", "chiante", "nique", "niquer", "salop", "salope", "salopard", "ntm"]
+  }
 };
