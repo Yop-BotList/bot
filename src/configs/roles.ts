73958ec302bee4b51c860567f51aacda0299880c
Nolhan#2508 <nolhan.dev@gmail.com>
2023-07-10 15:19:06 +0200

    Patched changes

--- src/configs/roles.ts
+++ src/configs/roles.ts
@@ -1,5 +1,5 @@
 export default {
-    verificator: "848550188095242261",
+    verificator: "782676062306959413",
     isclient: "784480914587254795",
     ticketsaccess: "829348127345344584",
     botintests: "783691786664673280",
@@ -9,6 +9,6 @@ export default {
     premiumbot: "793543495599915069",
     bypass: "783671210180345876",
     modrole: "832553606095568967",
-    partner: "828664698572374046",
+    partner: "908366583280304129",
     staffrole: "783669284207198218"
 }
